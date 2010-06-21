7ec760afb1d891823eaeccaa62cfb4210b7690c0
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-06-21 20:33:07 +0000

    Update inline docs for index.php to link to Hybrid's template hierarchy instead of the default WP template hierarchy.
    git-svn-id: http://svn.locallylost.com/themes/hybrid/trunk@90 dba0f204-706d-4bc1-bc29-8b92e0485636

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
  *
  * This template should not be shown.  It is a placeholder.
  * Specific templates for each type of content are available.
- * @link http://codex.wordpress.org/Template_Hierarchy
- * @link http://codex.wordpress.org/Stepping_Into_Templates
+ * @link http://themehybrid.com/themes/hybrid/template-hierarchy
  *
  * @package Hybrid
  * @subpackage Template
