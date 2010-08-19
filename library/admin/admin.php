6a5578b16b958cfd6523893e80c57cc5ee0b0513
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-08-19 04:35:28 +0000

    Make sure the theme supports admin features before loading them.
    git-svn-id: http://svn.locallylost.com/themes/hybrid/trunk@304 dba0f204-706d-4bc1-bc29-8b92e0485636

--- library/admin/admin.php
+++ library/admin/admin.php
@@ -4,7 +4,7 @@
  * launch the theme settings page and allow child themes and plugins to access theme-specific 
  * features. See meta-box.php for the post meta box functions.
  *
- * @package Hybrid
+ * @package HybridCore
  * @subpackage Admin
  */
 
@@ -20,14 +20,18 @@ add_action( 'init', 'hybrid_admin_init' );
 function hybrid_admin_init() {
 	$prefix = hybrid_get_prefix();
 
-	/* Initialize the theme settings page. */
-	add_action( 'admin_menu', 'hybrid_settings_page_init' );
+	if ( current_theme_supports( 'hybrid-core-theme-settings' ) ) {
 
-	/* Save settings page meta boxes. */
-	add_action( "{$prefix}_update_settings_page", 'hybrid_save_theme_settings' );
+		/* Initialize the theme settings page. */
+		add_action( 'admin_menu', 'hybrid_settings_page_init' );
+
+		/* Save settings page meta boxes. */
+		add_action( "{$prefix}_update_settings_page", 'hybrid_save_theme_settings' );
+	}
 
 	/* Add a new meta box to the post editor. */
-	add_action( 'admin_menu', 'hybrid_create_post_meta_box' );
+	if ( current_theme_supports( 'hybrid-core-post-meta-box' ) )
+		add_action( 'admin_menu', 'hybrid_create_post_meta_box' );
 
 	/* Load the admin stylesheet for the widgets screen. */
 	add_action( 'load-widgets.php', 'hybrid_settings_page_enqueue_style' );
