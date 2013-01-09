c7ccd0849948989dcc9f4004474a8b9ec0d9a540
Justin Tadlock <justin@justintadlock.com>
2013-01-08 19:09:23 -0600

    Version bump to 1.3-alpha.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@
  *
  * @package Hybrid
  * @subpackage Functions
- * @version 1.2.0
+ * @version 1.3.0
  * @author Justin Tadlock <justin@justintadlock.com>
  * @copyright Copyright (c) 2008 - 2013, Justin Tadlock
  * @link http://themehybrid.com/themes/hybrid
